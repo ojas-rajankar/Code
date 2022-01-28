c03d40c59eaa482316c9dd75d3cd5fd85059369d
ojas-rajankar <ojasrajankar365@gmail.com>
2022-01-29 00:53:07 +0530

    Work In Progress

---
+++ cryptocurrency-database/src/App.js
@@ -0,0 +1,220 @@
+import axios from "axios";
+import { useState } from "react";
+import styled from "styled-components";
+import "./App.css";
+import { motion } from "framer-motion"
+
+function App() {
+	const getData = () => {
+		const options = {
+			method: "GET",
+			url: "https://coinranking1.p.rapidapi.com/coins",
+			params: {
+				referenceCurrencyUuid: "yhjMzLPhuIDl",
+				timePeriod: "24h",
+				tiers: "1",
+				orderBy: "marketCap",
+				orderDirection: "desc",
+				limit: "50",
+				offset: "0",
+			},
+			headers: {
+				"x-rapidapi-key":
+					"ce99237786msh082e9fb626b28c4p1c36a7jsn43693defeed1",
+				"x-rapidapi-host":
+					"coinranking1.p.rapidapi.com",
+			},
+		};
+
+		axios
+			.request(options)
+			.then(function (response) {
+				const apiData = response.data.data.coins;
+				setData(apiData);
+			})
+			.catch(function (error) {
+				console.error(error);
+			});
+	};
+
+	
+
+	const [data, setData] = useState([""]);
+
+	const MainDiv = styled.div`
+		width: 100vw;
+		height: 100vh;
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background-color: #f977ce;
+background-image: linear-gradient(315deg, #f977ce 0%, #c373f2 74%);
+		z-index: -1;
+	`;
+
+	const Main = styled.div`
+		font-weight: 600;
+		z-index: 1;
+		font-family: "Raleway", sans-serif;
+		margin-top: 5rem;
+
+		ul {
+			list-style: none;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			padding: 0;
+		}
+
+		img {
+			height: calc(2rem + 2vw);
+			width: calc(2rem + 2vw);
+		}
+
+		.items {
+			width: 60vw;
+			margin: 3rem auto 0 auto;
+			padding: 1rem 0;
+			background-color: rgba(255, 255, 255, 0.1);
+			color: white;
+			border-radius: 5px;
+		}
+
+		button {
+			color: white;
+			font-size: calc(1rem + 0.3vw);
+			border: none;
+			background-color: rgba(255, 255, 255, 0.1);
+			padding: 0.5rem 1rem;
+			font-weight: 700;
+		}
+
+		button:hover {
+			background-color: rgba(255, 255, 255, 0.5);
+		}
+	`;
+
+	const Top = styled.div`
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		margin: -1rem calc(2rem + 2vw);
+		font-size: calc(1.2rem + 0.8vw);
+		align-items: center;
+		color: ${(props) => props.color};
+	`;
+
+	const Info = styled.div`
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+		margin: -1rem 1rem;
+		font-size: calc(0.7rem + 0.5vw);
+	`;
+
+	const Link = styled.div`
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		margin: 0.5rem 1rem 0rem 1rem;
+		font-size: calc(0.7rem + 0.5vw);
+		a {
+			text-decoration: none;
+			color: white;
+		}
+	`;
+
+	const Header = styled.div`
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		background-color: rgba( 255, 255, 255, 0.1);
+		height: 5rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		color: white;
+		font-size: calc(1rem + 0.3vw);
+	`;
+
+	return (
+		<div className="App">
+			<MainDiv />
+			<Main>
+				<Header>
+					<p>Top 50 Crypto Currencies Today</p>
+					<p>-- Ojas</p>
+				</Header>
+				<ul>
+					{data.map((items) => (
+						<motion.li
+						initial={{transform:"scale(0)"}}
+						animate={{scale:[0,1]}}
+						transition={{type:'spring', duration:1, delay: items.rank/2}}
+							className="items"
+							key={items.uuid}
+						>
+							<Top color={items.color}>
+								<p>
+									Rank:{" "}
+									{!items.rank
+										? 0
+										: items.rank}
+								</p>
+								<img
+									src={items.iconUrl}
+								></img>
+							</Top>
+							<Info>
+								<p>
+									Coin:{" "}
+									{!items.name
+										? "Name"
+										: items.name}
+								</p>
+								<p>
+									Symbol:{" "}
+									{!items.symbol
+										? "SYM"
+										: items.symbol}
+								</p>
+							</Info>
+							<Info>
+							<p>
+									Change Today:{" "}
+									{!items.change
+										? 0.0
+										: items.change}
+									%{" "}
+								</p>
+								<p>
+									Price: ${" "}
+									{!items.price
+										? 1000
+										:  Math.round((items.price) * 10000) / 10000}
+								</p>
+							</Info>
+							<Link color={items.color}>
+								<p>
+									<a
+										href={
+											items.coinrankingUrl
+										}
+									>
+										Click Here For More Information
+									</a>
+								</p>
+							</Link>
+						</motion.li>
+					))}
+				</ul>
+				<button onClick={getData}>Refresh Data</button>
+			</Main>
+		</div>
+	);
+}
+
+export default App;
--- random-quote-generator/src/App.js
+++ random-quote-generator/src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
+import "./App.css";
+import styled from "styled-components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const fetchQuote = () => {
+		const options = {
+			method: "POST",
+			url: "https://motivational-quotes1.p.rapidapi.com/motivation",
+			headers: {
+				"content-type": "application/json",
+				"x-rapidapi-key":
+					"ce99237786msh082e9fb626b28c4p1c36a7jsn43693defeed1",
+				"x-rapidapi-host":
+					"motivational-quotes1.p.rapidapi.com",
+			},
+			data: { key1: "value"},
+		};
+
+		axios
+			.request(options)
+			.then(function (response) {
+				setQuote(response.data);
+			})
+			.catch(function (error) {
+				console.error(error);
+			});
+	};
+
+	const Main = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url("https://www.freecodecamp.org/news/content/images/size/w2000/2021/06/w-qjCHPZbeXCQ-unsplash.jpg");
+  `;
+
+  const Quote = styled.div`
+    max-width: 60%;
+    width: auto;
+    border: 0.5px solid white;
+    height: auto;
+    padding: 2rem 1rem;
+    color: white;
+    font-weight: 600;
+    background-color: rgba(0, 0, 0, 0.2);
+    border-radius: 10px;
+
+    h1{
+      font-size: calc(1rem + 1vw);
+    }
+
+    button {
+      width: auto;
+      height: auto;
+      font-weight: 800;
+      border: 0.1px solid white;
+      background-color: rgba(0,0,0,0);
+      color: white;
+      font-size: calc(1rem + 0.2vw);
+      padding: 5px 18px;
+      transition: all 0.5s 0s ease;
+      margin-top: 0.5rem;
+    }
+
+
+    button:hover {
+      background-color: white;
+      color: rgba(0,0,0, 1);
+    }
+  `
+
+	const [quote, setQuote] = useState(
+		"Click Below To Get Random Quote",
+	);
+
+	return (
+		<div className="App">
+			<Main>
+        <Quote>
+          <h1>{quote}</h1>
+          <button onClick={fetchQuote}>Get Quote</button>
+        </Quote>
+      </Main>
+		</div>
+	);
 }
 
 export default App;
